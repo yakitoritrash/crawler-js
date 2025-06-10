181f710fdb639330efb4763c56afe96cbe1c1364
Arch me <kishudwivedi30@gmail.com>
2025-06-10 18:58:54 +0530

    url extraction

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import normalizeURL from './crawl'
+import { normalizeURL, getURLsFromHTML } from './crawl'
 
 
 test('normalizeURL strip protocol', () => {
@@ -30,5 +30,22 @@ test('normalizeURL different protocol', () => {
   const input = 'http://example.com/path'
   const actual = normalizeURL(input)
   const expected = 'example.com/path'
+  expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML', () => {
+  const inputHTML = `
+  <html>
+    <body>
+      <a href "https://example.com/">
+        Hello
+      </a>
+    </body>
+  </html>
+  `
+  const inputbaseURL = "https://example.com"
+  const actual = getURLsFromHTML(inputHTML, inputbaseURL)
+  const expected = ["https://example.com/"]
   expect(actual).toEqual(expected)
 })
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,8 +1,13 @@
-export default function normalizeURL(urlString: string): string {
+export function normalizeURL(urlString: string): string {
   const urlObj = new URL(urlString);
   const hostname = `${urlObj.hostname}${urlObj.pathname}`;
   if (hostname.length > 0 && hostname.slice(-1) === '/') {
     return hostname.slice(0, -1);
   }
   return hostname;
+}
+
+export function getURLsFromHTML(html: string, baseURL: string): string[] {
+  const urls: string[] = [];
+  return urls;
 }
